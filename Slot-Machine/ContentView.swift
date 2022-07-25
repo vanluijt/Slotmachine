fc720fc25060efa9b8d78fc4eb60c0bc2df39c29
vanluijt <vl54@ziggo.nl>
2022-07-25 16:27:44 +0200

    Initial Commit

---
+++ Slot-Machine/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Slot-Machine
+//
+//  Created by P.A. van Luijt on 25/07/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
