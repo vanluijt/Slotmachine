fc911b0613d731ad2b75278b8f38539139e51ede
vanluijt <vl54@ziggo.nl>
2022-08-05 12:42:22 +0200

    emmer control

--- Slot-Machine/ContentView.swift
+++ Slot-Machine/ContentView.swift
@@ -8,9 +8,50 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var creditwaarde = 20
+    @State var a: Int = 1
+    @State var b: Int = 2
+    @State var c: Int = 3
+    
     var body: some View {
-        Text("Hello, world!")
+        VStack {
+            Spacer()
+            Text("Emmer Slots")
+                .font(.largeTitle)
+            Spacer()
+           
+            HStack {
+                Text("credits   \(creditwaarde)").font(.largeTitle)
+                   
+                  }
+            Spacer()
+            
+            HStack {
+                Image("fruit\(a)").resizable().aspectRatio(contentMode: .fit)
+                Image("fruit\(b)").resizable().aspectRatio(contentMode: .fit)
+                Image("fruit\(c)").resizable().aspectRatio(contentMode: .fit)
+            }
+            
+            
+            Spacer()
+            Button("Draai") {
+                a = Int.random(in: 1...3)
+                b = Int.random(in: 1...3)
+                c = Int.random(in: 1...3)
+                if (a==b) && (b==c) {
+                    creditwaarde += 10
+                }
+                else { creditwaarde -= 1
+                }
+            }
             .padding()
+               .padding([.leading,.trailing], 40)
+                .foregroundColor(.white)
+                .background(Color(.systemPink))
+                .cornerRadius(25)
+                .font(.system(size: 18,weight: .bold,design: .default))
+                Spacer()
+        }
     }
 }
 
